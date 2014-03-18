48ad95a1ba742ae82f4ccffcc3390bc804615fcf
Nick Langridge <nicklangridge@hotmail.com>
2014-03-18 14:22:16 +0000

    Use new LoadPlugins and fix paths

--- utils/locate_species.pl
+++ utils/locate_species.pl
@@ -19,16 +19,9 @@
 use strict;
 use FindBin qw($Bin);
 use Data::Dumper;
-
-BEGIN {
-  unshift @INC, "$Bin/../../../conf";
-  unshift @INC, "$Bin/../../../";
-  require SiteDefs;
-  unshift @INC, $_ for @SiteDefs::ENSEMBL_LIB_DIRS;
-}
-
-require LoadPlugins;
-LoadPlugins::plugin(sub {/(SpeciesDefs.pm)$/});
+use lib "$Bin/../../eg-web-common/utils";
+use LibDirs;
+use LoadPlugins;
 
 use EnsEMBL::Web::SpeciesDefs;
 my $sd = EnsEMBL::Web::SpeciesDefs->new;
