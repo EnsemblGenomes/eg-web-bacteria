04d9d3d27ce8c5d89917307d875709f2e5f6f066
Nick Langridge <nicklangridge@hotmail.com>
2014-10-28 08:51:41 +0000

    Fix code that changes blast species case

--- htdocs/components/20_BlastSpeciesList_EGB.js
+++ htdocs/components/20_BlastSpeciesList_EGB.js
@@ -1,8 +1,9 @@
-Ensembl.Panel.BlastSpeciesList = Ensembl.Panel.extend({
+Ensembl.Panel.BlastSpeciesList = Ensembl.Panel.BlastSpeciesList.extend({
   // for Bacteria, make sure blast species names are all-lower-case 
   updateTaxonSelection: function(items) {
-  	this.base.apply(this, arguments);
-    this.elLk.checkboxes.filter('input[type=checkbox]').each( function(ele) {
+  	var panel = this; 
+    panel.base.apply(this, arguments);
+    $('input[type=checkbox]', panel.elLk.checkboxes).each( function(ele) {
       $(this).attr('value', $(this).attr('value').toLowerCase());
     });
   }
