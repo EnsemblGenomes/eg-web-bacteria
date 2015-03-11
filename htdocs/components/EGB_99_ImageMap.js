18e665d7733a8874c321156833f8825e64d44511
Nick Langridge <nicklangridge@hotmail.com>
2015-03-11 16:38:11 +0000

    Update to E79

--- htdocs/components/EGB_99_ImageMap.js
+++ htdocs/components/EGB_99_ImageMap.js
@@ -5,25 +5,27 @@ Ensembl.Panel.ImageMap =  Ensembl.Panel.ImageMap.extend({
   makeImageMap: function () {
     var panel = this;
     
-    var highlight = !!( window.location.pathname.match(/\/Location\/|\/Variation_Gene\/Image/) && !this.vdrag);
+// EG    
+    var highlight = !!(window.location.pathname.match(/\/Location\/|\/Variation_(Gene|Transcript)\/Image/) && !this.vertical);
+//  
     var rect = [ 'l', 't', 'r', 'b' ];
     var speciesNumber, c, r, start, end, scale;
     
-    this.elLk.areas.each(function () {
+    $.each(this.elLk.areas,function () {
       c = { a: this };
       
       if (this.shape && this.shape.toLowerCase() !== 'rect') {
         c.c = [];
-        $.each(this.coords.split(/[ ,]/), function () { c.c.push(parseInt(this, 10)); });
+        $.each(this.coords, function () { c.c.push(parseInt(this, 10)); });
       } else {
-        $.each(this.coords.split(/[ ,]/), function (i) { c[rect[i]] = parseInt(this, 10); });
+        $.each(this.coords, function (i) { c[rect[i]] = parseInt(this, 10); });
       }
       
       panel.areas.push(c);
       
-      if (this.className.match(/drag/)) {
+      if (this.klass.drag || this.klass.vdrag) {
         // r = [ '#drag', image number, species number, species name, region, start, end, strand ]
-        r     = c.a.href.split('|');
+        r     = c.a.attrs.href.split('|');
         start = parseInt(r[5], 10);
         end   = parseInt(r[6], 10);
         
@@ -32,9 +34,9 @@ Ensembl.Panel.ImageMap =  Ensembl.Panel.ImageMap.extend({
  	      var midpointImg = parseInt(r[9]) || 0;
 
         if(start <= end) {
-          scale = (end - start + 1) / (c.r - c.l);                   // bps per pixel on image
+          scale = (end - start + 1) / (this.vertical ? (c.b - c.t) : (c.r - c.l));                   // bps per pixel on image
         } else {
-          scale = (end + midpointChr - start + 1) / (c.r - c.l);     // bps per pixel on image
+          scale = (end + midpointChr - start + 1) / (this.vertical ? (c.b - c.t) : (c.r - c.l));     // bps per pixel on image
         }
  
         c.range = { start: start, end: end, scale: scale, midchr: midpointChr, midimg: midpointImg };
@@ -43,7 +45,7 @@ Ensembl.Panel.ImageMap =  Ensembl.Panel.ImageMap.extend({
         panel.draggables.push(c);
         
         if (highlight === true) {
-          r = this.href.split('|');
+          r = this.attrs.href.split('|');
           speciesNumber = parseInt(r[1], 10) - 1;
           
           if (panel.multi || !speciesNumber) {
@@ -61,24 +63,68 @@ Ensembl.Panel.ImageMap =  Ensembl.Panel.ImageMap.extend({
         }
       }
     });
-    
+
+    if (this.draggables.length) {
+      this.labelRight = this.draggables[0].l;  // label ends where the drag region starts
+    }
+
     if (Ensembl.images.total) {
       this.highlightAllImages();
     }
       
     this.elLk.drag.on({
       mousedown: function (e) {
-        // Only draw the drag box for left clicks.
-        // This property exists in all our supported browsers, and browsers without it will draw the box for all clicks
-        if (!e.which || e.which === 1) {
+
+        if (!e.which || e.which === 1) { // Only draw the drag box for left clicks.
           panel.dragStart(e);
         }
         
         return false;
       },
-      click: function (e) {
+      mousemove: function(e) {
+
+        if (panel.dragging !== false) {
+          return;
+        }
+        
+        var area = panel.getArea(panel.getMapCoords(e));
+        var tip;
+
+        // change the cursor to pointer for clickable areas
+        $(this).toggleClass('drag_select_pointer', !(!area || area.a.klass.label || area.a.klass.drag || area.a.klass.vdrag || area.a.klass.hover));
+
+        // Add helptips on navigation controls in multi species view
+        if (area && area.a && area.a.klass.nav) {
+          if (tip !== area.a.attrs.alt) {
+            tip = area.a.attrs.alt;
+            
+            if (!panel.elLk.navHelptip) {
+              panel.elLk.navHelptip = $('<div class="ui-tooltip helptip-bottom"><div class="ui-tooltip-content"></div></div>');
+            }
+            
+            panel.elLk.navHelptip.children().html(tip).end().appendTo('body').position({
+              of: { pageX: panel.imgOffset.left + area.l + 10, pageY: panel.imgOffset.top + area.t - 48, preventDefault: true }, // fake an event
+              my: 'center top'
+            });
+          }
+        } else {
+          if (panel.elLk.navHelptip) {
+            panel.elLk.navHelptip.detach().css({ top: 0, left: 0 });
+          }
+        }
+      },
+      mouseleave: function(e) {
+        if (e.relatedTarget) {
+
+          if (panel.elLk.navHelptip) {
+            panel.elLk.navHelptip.detach();
+          }
+
+        }
+      },
+      click: function (e, e2) {
         if (panel.clicking) {
-          panel.makeZMenu(e, panel.getMapCoords(e));
+          panel.makeZMenu(e2 || e, panel.getMapCoords(e2 || e));
         } else {
           panel.clicking = true;
         }
