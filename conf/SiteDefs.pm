777d3278717aaf6021575bb033ab35b782894a87
Nick Langridge <nicklangridge@hotmail.com>
2015-05-28 10:06:24 +0100

    Add new collections

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -38,7 +38,7 @@ sub update_conf {
     map {delete($SiteDefs::__species_aliases{$_}) } keys %SiteDefs::__species_aliases;
   
     $SiteDefs::ENSEMBL_DATASETS = [];
-    push (@{$SiteDefs::ENSEMBL_DATASETS}, "bacteria_$_") foreach (0..90);
+    push (@{$SiteDefs::ENSEMBL_DATASETS}, "bacteria_$_") foreach (0..95);
 
     #$SiteDefs::ENSEMBL_DATASETS = ['bacteria_0'];
     
