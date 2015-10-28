04541897033738f29c17ec052b0535ef2295604b
Nick Langridge <nicklangridge@hotmail.com>
2015-10-28 16:44:27 +0000

    Update tool links

--- modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
@@ -21,44 +21,12 @@ package EnsEMBL::Web::Document::Element::ToolLinks;
 ### Generates links to site tools - BLAST, help, login, etc (currently in masthead)
 
 use strict;
+use previous qw(links);
 
-
-sub content {
-  my $self    = shift;
-  my $hub     = $self->hub;
-  my $species = $hub->species;
-     $species = !$species || $species eq 'Multi' || $species eq 'common' ? 'Multi' : $species;
-  my @links; # = sprintf '<a class="constant" href="%s">Home</a>', $self->home;
-###EG  
-  if ($self->hub->species_defs->ENSEMBL_ENASEARCH_ENABLED) {
-      push @links,   '<a class="constant" href="/Multi/enasearch">Sequence Search</a>';
-  }
-
-  if ($self->hub->species_defs->ENSEMBL_BLAST_ENABLED) {
-      push @links, qq{<a class="constant" href="/$species/blastview">BLAST</a>}
-  }
-
-###
-  push @links,   '<a class="constant" href="/tools.html">Tools</a>';
-  push @links,   '<a class="constant" href="/downloads.html">Downloads</a>';
-### EG
-#  push @links,   '<a class="constant" href="/info/">Help &amp; Documentation</a>';
-  push @links,   '<a class="constant" href="/info/website/index.html">Help</a>';
-  push @links,   '<a class="constant" href="http://www.ensemblgenomes.org/info">Documentation</a>';
-###
-
-  push @links,   '<a class="constant modal_link" href="/Help/Mirrors">Mirrors</a>' if keys %{$hub->species_defs->ENSEMBL_MIRRORS || {}};
-
-  my $last  = pop @links;
-  my $tools = join '', map "<li>$_</li>", @links;
-  
-  return qq{
-    <ul class="tools">$tools<li class="last">$last</li></ul>
-    <div class="more">
-      <a href="#">More <span class="arrow">&#9660;</span></a>
-    </div>
-  };
+sub links {
+  return [ grep {$_ !~ /biomart/} @{ shift->PREV::links} ];
 }
+
 
 
 1;
