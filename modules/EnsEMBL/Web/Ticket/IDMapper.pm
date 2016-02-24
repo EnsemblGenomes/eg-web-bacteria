d51516abe66ea0ac21169ed54dfc8e6ad8eb42da
Sanjay Boddu <sboddu@ebi.ac.uk>
2016-02-24 10:14:57 +0000

    Part of ENSEMBL-4284 - Tools db not accepting assembly as null for ID history converter- Related to cb618ac3ffa0632a4d03dca3618bf113f995932b

---
+++ modules/EnsEMBL/Web/Ticket/IDMapper.pm
@@ -0,0 +1,61 @@
+=head1 LICENSE
+
+Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Ticket::IDMapper;
+
+use strict;
+use warnings;
+
+use List::Util qw(first);
+
+use EnsEMBL::Web::Exceptions;
+use EnsEMBL::Web::Job::IDMapper;
+
+use parent qw(EnsEMBL::Web::Ticket);
+
+sub init_from_user_input {
+  ## Abstract method implementation
+  my $self      = shift;
+  my $hub       = $self->hub;
+  my $species   = $hub->param('species');
+  my $method    = first { $hub->param($_) } qw(file url text);
+
+  # if no data entered
+  throw exception('InputError', 'No input data has been entered') unless $method;
+
+  my $description = $hub->param('name') || sprintf('ID mapping of %s', $method eq 'text' ? 'pasted data' : ($method eq 'url' ? 'data from URL' : sprintf("%s", $hub->param('file'))));
+
+  my ($file_content, $file_name) = $self->get_input_file_content($method);
+
+  $self->add_job(EnsEMBL::Web::Job::IDMapper->new($self, {
+    'job_desc'    => $description,
+    'species'     => $species,
+## EG - Assembly can be empty for Bacteria
+    'assembly'    => $hub->species_defs->get_config($species, 'ASSEMBLY_VERSION') || '',
+## EG
+    'job_data'    => {
+      'species'     => $species,
+      'input'       => { 'type' => $method, 'url' => $hub->param('url') || '', 'file' => $file_name }, # save this info to pre-populate fields when editing existing job
+      'input_file'  => $file_name
+    }
+  }, {
+    $file_name    => {'content' => $file_content}
+  }));
+}
+
+1;
