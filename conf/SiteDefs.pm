83319067be16d6dfc3a39c813c525ea3e6a47ec4
Zhicheng-Liu <zl@ebi.ac.uk>
2017-08-03 15:30:08 +0100

    Decrease the MAX_PROCESS_SIZE for Bacteria
    As with slimmer registry, the memory requirements for each child process is lowered:
    https://github.com/EnsemblGenomes/eg-web-bacteria/commit/f36fc9709c24f1bcc8867e91f09d87cc6922be23
    
    Decrease this setting to better manage the process size.

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -48,7 +48,7 @@ sub update_conf {
     $SiteDefs::ENSEMBL_SECONDARY_SPECIES = 'Tropheryma_whipplei_tw08_27'; 
     $SiteDefs::__species_aliases{ 'Tropheryma_whipplei_tw08_27'       } = [qw(tw)];
 
-    $SiteDefs::ENSEMBL_MAX_PROCESS_SIZE = 2800000; # Kill httpd over 2,800,000KB
+    $SiteDefs::ENSEMBL_MAX_PROCESS_SIZE = 2500000; # Kill httpd over 2,500,000KB
     $SiteDefs::ENSEMBL_MAX_CLIENTS      = 12;      # Limit child processes to 12
     
     $SiteDefs::ENSEMBL_HMMER_ENABLED = 1;
