2e09d0de897063edc2f94cd8a1c6a8a8f7eb1688
Anne Lyle <ens-ap5@users.noreply.github.com>
2022-08-25 15:46:35 +0100

    Remove obsolete keywords

--- modules/EnsEMBL/Web/Document/Element/Meta.pm
+++ modules/EnsEMBL/Web/Document/Element/Meta.pm
@@ -21,7 +21,6 @@ package EnsEMBL::Web::Document::Element::Meta;
 sub init {
   my $self = shift;
   $self->add('description', 'Ensembl Bacteria is a genome-centric portal for bacterial species of scientific interest');
-  $self->add('keywords', 'Ensembl, Ensembl Genomes, genome, genome browser, comparative genomics, variation, SNPs, EST, mRNA, rna-Seq, orthologs, paralogs, synteny, assembly, genes, transcripts, translations, proteins, Escherichia coli, E.coli, Bacillus subtillis, B. subtilis, archaea, bacteria');
 }
 
 1;
